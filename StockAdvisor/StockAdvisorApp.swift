83ecc3f3f7a72a92f0c33dd639e6296379b2e3fb
davidakoontz <david@koontz.name>
2022-03-30 19:05:18 -0500

    Initial Commit

---
+++ StockAdvisor/StockAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAdvisorApp.swift
+//  StockAdvisor
+//
+//  Created by David on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct StockAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
